49d343a6ca07770bb5b3cb69d4c99eba6399aff1
Bobby Borszich <bobby.borszich@carvana.com>
2016-10-16 15:13:52 -0700

    adding feature c

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+        }
+
+        private void FeatureC()
+        {
+            // adding feature c
         }
     }
 }
