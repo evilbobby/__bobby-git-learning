c34562b88abddfa2c4009d2391ddfd0d4ac65475
Bobby Borszich <bobby.borszich@gmail.com>
2016-10-16 15:35:18 -0700

    Develop to master (#3)
    * moving feature a to dev (#1)
    
    * adding code to feature a
    
    * adding more to feature a
    
    * shared component to feature a
    
    * feature a adding rev 1
    
    * pulling b to dev (#2)
    
    * adding feature b
    
    * adding more to feature b
    
    * adding shared component from feature b

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
+            //adding stuff to feature a
+        }
+
+        private void FeatureA()
+        {
+            // adding to feature a
+
+            // adding rev 1
+        }
+
+        private void FeatureA_SharedComponent()
+        {
+            // this is a shared component from Feature A
+        }
+
+
+
+        private void FeatureB()
+        {
+            //this is feature b
+            // another revision on feature b
+        }
+
+        private void FeatureB_SharedComponent()
+        {
+            // adding shared component for feature b
         }
     }
 }
